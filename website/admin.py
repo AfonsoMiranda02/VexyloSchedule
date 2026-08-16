4f191ed4576c0f22dc06fb8373ad5d1963e6e43b
AfonsoMiranda02 <afonsorm4321@gmail.com>
2026-08-16 20:51:12 +0100

    Feat: Personaliza UserAdmin para ocultar permissões complexas de contas não-superuser

--- website/admin.py
+++ website/admin.py
@@ -1,6 +1,29 @@
 from django.contrib import admin
 from django.utils.html import format_html
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 from .models import BusinessInfo, ServiceCategory, Service, Appointment, StaffMember, Testimonial
+
+class CustomUserAdmin(UserAdmin):
+    def get_fieldsets(self, request, obj=None):
+        fieldsets = super().get_fieldsets(request, obj)
+        if not request.user.is_superuser:
+            # Create a new fieldsets tuple without 'user_permissions' and 'is_superuser'
+            new_fieldsets = []
+            for name, opts in fieldsets:
+                if name == 'Permissions':
+                    # Filter out 'user_permissions' and 'is_superuser'
+                    fields = tuple(f for f in opts.get('fields', []) if f not in ('user_permissions', 'is_superuser'))
+                    if fields:
+                        new_fieldsets.append((name, {'fields': fields}))
+                else:
+                    new_fieldsets.append((name, opts))
+            return tuple(new_fieldsets)
+        return fieldsets
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
+
 
 @admin.register(BusinessInfo)
 class BusinessInfoAdmin(admin.ModelAdmin): 
